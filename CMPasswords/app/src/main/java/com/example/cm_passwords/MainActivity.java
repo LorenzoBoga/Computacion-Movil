fd84ad64a9208da5916db35a82632a7a520dd6d1
LorenzoBoga <lorenzoboga10@gmail.com>
2022-05-11 18:49:50 +0200

    subo programa basico

---
+++ CMPasswords/app/src/main/java/com/example/cm_passwords/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.cm_passwords;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button botonEntrar = findViewById(R.id.enter_button);
+        
+    }
+}
